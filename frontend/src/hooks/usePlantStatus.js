a9095700d6dd133338f30d0d5cbcafcbf047ffe6
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-05-31 23:09:57 +0200

    refactor(typescript): remove JavaScript plant status hook

--- frontend/src/hooks/usePlantStatus.js
+++
@@ -1,8 +0,0 @@
-import { useMemo } from "react";
-import { calculatePlantStatus } from "../domain/plantStatus";
-
-export function usePlantStatus(plant, season) {
-  return useMemo(() => {
-    return calculatePlantStatus(plant, season);
-  }, [plant, season]);
-}
